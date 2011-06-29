30c755f22e9000e91786cdc5ff9a4e97815822b6
karpet <karpet@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2011-06-29 04:14:58 +0000

    release 0.03

--- lib/SWISH/Prog/Lucy.pm
+++ lib/SWISH/Prog/Lucy.pm
@@ -2,7 +2,7 @@ package SWISH::Prog::Lucy;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 NAME
 
--- lib/SWISH/Prog/Lucy/InvIndex.pm
+++ lib/SWISH/Prog/Lucy/InvIndex.pm
@@ -2,7 +2,7 @@ package SWISH::Prog::Lucy::InvIndex;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use base qw( SWISH::Prog::InvIndex );
 
