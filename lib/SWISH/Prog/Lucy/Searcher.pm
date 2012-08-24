6e1181588dc7b300cecfc80edfdec464c8ddbde9
karpet <karpet@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2012-08-24 16:53:34 +0000

    use meta_file instead of meta->file

--- lib/SWISH/Prog/Lucy/Searcher.pm
+++ lib/SWISH/Prog/Lucy/Searcher.pm
@@ -89,7 +89,7 @@ sub init {
     # cache the meta file stat(), to test if it changes
     # while the searcher is open. See get_lucy()
     $self->{swish_xml}
-        = Path::Class::File::Stat->new( $invindex->meta->file );
+        = Path::Class::File::Stat->new( $invindex->meta_file );
     $self->{swish_xml}->use_md5();    # slower but better
     $self->{_uuid} = $config->Index->{UUID} || "LUCY_NO_UUID";
 
